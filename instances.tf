fd488c132e1d6b742a199d513d631a499446bcc0
Thomas-Gadois <115066194+Thomas-Gadois@users.noreply.github.com>
2024-10-07 18:53:40 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "GRP2-EC2" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.GRP2-public-subnet[0].id # Placer l'instance dans le premier sous-réseau public
   vpc_security_group_ids = [aws_default_security_group.GRP2-security-rule.id]
-  key_name               = "GRP2-Key"
+  #key_name               = "GRP2-Key" # Clé privé pour Californie
 
   tags = {
     Name = "GRP2-EC2"
